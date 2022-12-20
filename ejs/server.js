d434c15a9d6564cbb99fd1bade625cc39b602bc9
gonza-ro <gonzalorp12345@gmail.com>
2022-12-20 20:48:49 -0300

    espero que funcione el ejs...me falta hacer el handle

---
+++ ejs/server.js
@@ -0,0 +1,36 @@
+import express from 'express'
+
+// Declarar constantes y arrays
+let productos = []
+
+const app = express();
+
+// Funcionalidades de nuestro servidor
+app.use(express.urlencoded({extended: true}))
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/formulario', (req, res) => {
+    res.render('form', {productos});
+});
+
+app.get('/table', (req, res) => {
+    res.render('form1', {productos});
+});
+
+app.post('/datos', (req, res) => {
+    productos.push(req.body)
+    console.log(productos)
+    res.redirect('/table')
+});
+
+
+
+
+/* Configurar el servidor */
+const PORT = 8080;
+const server = app.listen(PORT, () => {
+    console.log('Servidor corriendo en el puerto: ', PORT);
+})
+server.on('error', error => console.log('Error en el servidor: ', error));
